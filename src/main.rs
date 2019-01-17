35df3f52e2b3608b6d4193066b4147175953f1fe
sts10 <sschlinkert@gmail.com>
2019-01-17 18:34:42 -0500

    looking good, more debug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+extern crate reqwest;
+extern crate rpassword;
+extern crate sha1;
+
+// Securely read a password and query the Pwned Passwords API to
+// determine if it's been breached ever.
+
 fn main() {
-    println!("Hello, world!");
+    let pass = rpassword::prompt_password_stdout("Password: ").unwrap();
+    // dbg!(&pass);
+    eprintln!("pass is {}", pass);
+
+    let digest = sha1::Sha1::from(pass).digest().to_string().to_uppercase();
+    eprintln!("digest is {}", digest);
+    let (prefix, suffix) = (&digest[..5], &digest[5..]);
+
+    // API requires us to submit just the first 5 characters of the hash
+
+    let url = format!("https://api.pwnedpasswords.com/range/{}", prefix);
+    let mut response = reqwest::get(&url).unwrap();
+
+    let body = response.text().unwrap();
+
+    eprintln!("body is {}", body);
+
+    // Reponse is a series of lines like
+    //
+    //  suffix:N
+    //
+    // Where N is the number of times that password has appeared.
+
+    for line in body.lines() {
+        let mut split = line.split(':');
+        if split.next().unwrap() == suffix {
+            println!("{} matches found.", split.next().unwrap());
+            return;
+        }
+    }
+
+    println!("No matches found.");
+    return;
 }
