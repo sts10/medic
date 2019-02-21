a8ea07e57493f03e472b3385338f35f8f4e60d5a
sts10 <sschlinkert@gmail.com>
2019-02-21 18:51:59 -0500

    adds new tests for keepass db that does NOT require a keyfile

--- src/lib.rs
+++ src/lib.rs
@@ -399,17 +399,16 @@ pub fn get_file_path() -> io::Result<String> {
 mod integration_tests {
     use super::*;
 
-    fn make_test_entries_from_keepass_database() -> Vec<Entry> {
+    fn make_test_entries_from_keepass_database_requiring_keyfile() -> Vec<Entry> {
         let keepass_db_file_path = "test-files/test_db.kdbx".to_string();
         let test_db_pass = "password".to_string();
         let test_keyfile = Some("test-files/test_key_file");
-        // build_entries_from_keepass_db(&keepass_db_file_path, test_db_pass, None)
         build_entries_from_keepass_db(&keepass_db_file_path, test_db_pass, test_keyfile)
     }
 
     #[test]
     fn can_check_keepass_db_against_haveibeenpwned_api_online() {
-        let entries = make_test_entries_from_keepass_database();
+        let entries = make_test_entries_from_keepass_database_requiring_keyfile();
         let breached_entries = check_database_online(&entries);
         assert_eq!(breached_entries.len(), 3);
     }
@@ -418,7 +417,7 @@ mod integration_tests {
     // a real long time
     #[test]
     fn can_check_keepass_db_against_offline_list_of_hashes() {
-        let entries = make_test_entries_from_keepass_database();
+        let entries = make_test_entries_from_keepass_database_requiring_keyfile();
         let passwords_file_path =
             "../hibp/pwned-passwords-sha1-ordered-by-count-v4.txt".to_string();
 
@@ -429,7 +428,42 @@ mod integration_tests {
 
     #[test]
     fn can_make_a_digest_map_from_keepass_database() {
-        let entries = make_test_entries_from_keepass_database();
+        let entries = make_test_entries_from_keepass_database_requiring_keyfile();
+
+        let digest_map = make_digest_map(&entries).unwrap();
+
+        let mut number_of_entries_with_duplicate_passwords = 0;
+        for groups in digest_map.values() {
+            if groups.len() > 1 {
+                for _entry in groups {
+                    number_of_entries_with_duplicate_passwords += 1;
+                }
+            }
+        }
+
+        assert_eq!(number_of_entries_with_duplicate_passwords, 2);
+    }
+
+    // #######################################################################
+    // # Some tests using a KeePass database that does NOT require a keyfile #
+    // #######################################################################
+
+    fn make_test_entries_from_keepass_database_not_requiring_keyfile() -> Vec<Entry> {
+        let keepass_db_file_path = "test-files/test_db_no_keyfile.kdbx".to_string();
+        let test_db_pass = "password".to_string();
+        build_entries_from_keepass_db(&keepass_db_file_path, test_db_pass, None)
+    }
+
+    #[test]
+    fn can_check_keepass_db_that_does_not_require_a_keyfile_against_haveibeenpwned_api_online() {
+        let entries = make_test_entries_from_keepass_database_not_requiring_keyfile();
+        let breached_entries = check_database_online(&entries);
+        assert_eq!(breached_entries.len(), 3);
+    }
+
+    #[test]
+    fn can_make_a_digest_map_from_keepass_database_that_does_not_require_a_keyfile() {
+        let entries = make_test_entries_from_keepass_database_not_requiring_keyfile();
 
         let digest_map = make_digest_map(&entries).unwrap();
 
